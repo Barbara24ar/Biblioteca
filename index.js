61c79b7749aaf7911e97e9a54587a2369e6cedf6
barbara <barbara.rolon.rivero@gmail.com>
2025-02-08 17:44:24 -0500

    manejo de otro tipo de errores

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ function manejarError(error){
     throw new Error(chalk.red(error))
 }
 
-function cargarArchivo(rutaArchivo){
-    const enconding = "utf-8"
-    fs.promises.readFile(rutaArchivo, enconding)
-    .then((texto) => console.log(chalk.green(texto)))
-    .catch((error) => manejarError(error))
+async function cargarArchivo(rutaArchivo) {
+    try{
+        const encoding = "utf-8"
+        const texto = await fs.promises.readFile(rutaArchivo, encoding)
+        console.log(chalk.green(texto))
+    }catch(error){
+        manejarError(error)
+    }
 }
 
 
