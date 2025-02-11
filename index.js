47c3cf840ec1191c461646f24ae27c858355635b
barbara <barbara.rolon.rivero@gmail.com>
2025-02-10 19:52:46 -0500

    trabajando con expresiones regulares

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+
+
+function obtenerEnlaces(texto) {
+    const regex = /\[([^\[\]]*?)\]\((https?:\/\/[^\s?#.][^\s]*)\)/gm
+    const enlaces = [...texto.matchAll(regex)]
+    const resultados = enlaces.map(enlace => ({ [enlace[1]]: enlace[2] }))
+    console.log(resultados)
+}
+
 function manejarError(error){
     console.log(error)
     throw new Error(chalk.red(error))
 }
 
+
 async function cargarArchivo(rutaArchivo) {
     try{
         const encoding = "utf-8"
         const texto = await fs.promises.readFile(rutaArchivo, encoding)
-        console.log(chalk.green(texto))
+        const resultados = obtenerEnlaces(texto)
+        console.log(resultados)
     }catch(error){
         manejarError(error)
     }
 }
-
 
 cargarArchivo("./archivos/texto.md") 
